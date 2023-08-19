ad265d2d6b11b6dff148ab176c53dc2ad8d1edf1
DarKS4ys <melihyardim1057@gmail.com>
2023-08-19 13:53:58 +0300

    update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,6 +9,7 @@ import { RiDashboardLine } from 'react-icons/ri';
 import { BiMessageAlt } from 'react-icons/bi';
 import { RiSettings3Line } from 'react-icons/ri';
 import { BiBook } from 'react-icons/bi';
+import { VscLibrary } from 'react-icons/gr';
 
 const montserrat = Montserrat({ weight: '600', subsets: ['latin'] });
 
@@ -25,8 +26,13 @@ const routes = [
   },
   {
     label: 'Courses',
-    icon: BiBook,
+    icon: VscLibrary,
     href: '/courses',
+  },
+  {
+    label: 'Resources',
+    icon: BiBook,
+    href: '/resources',
   },
   {
     label: 'Settings',
@@ -40,7 +46,7 @@ const Sidebar = () => {
     return (
       <div className='fixed left-0 h-full min-h-screen justify-center p-1 items-center w-[15rem] bg-neutral-900 border-r text-white'>
         <div className="px-3 py-2 flex-1">
-          <Link href="/dashboard" className="flex items-center text-center ml-4 my-4">
+          <Link href="/dashboard" className="flex items-center text-center ml-4 my-6">
             <h1
               className={clsx('mt-1 text-3xl font-semibold tracking-widest', montserrat.className)}
             >
@@ -53,7 +59,7 @@ const Sidebar = () => {
                 href={route.href}
                 key={route.href}
                 className={clsx(
-                  'text-base sm:text-lg group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+                  'text-base sm:text-lg group flex p-3 w-full justify-start  font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
                     pathname === route.href
                         ? 'text-white bg-white/10'
                         : 'text-zinc-400'
