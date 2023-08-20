2e048ea9f1a3d43b864d3a38041ca2742df33417
DarKS4ys <melihyardim1057@gmail.com>
2023-08-20 09:21:54 +0300

    added email auth

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,107 +1,42 @@
 "use client"
 
-import { useState } from 'react'
-import Input from './Input'
-import Button from './Button'
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
-import { AiOutlineGoogle } from 'react-icons/ai'
-import { auth } from '@/utils/firebase'
-
-export default function Login() {
-
-    const [user, setUser] = useState({})
-
-    const [registerEmail, setRegisterEmail] = useState("")
-    const [registerPassword, setRegisterPassword] = useState("")
-
-    const [loginEmail, setLoginEmail] = useState("")
-    const [loginPassword, setLoginPassword] = useState("")
-
-    const [isLoginMode, setIsLoginMode] = useState(false); // Add this state
+import React, { ReactElement, useState } from 'react';
+import type { IconType } from 'react-icons';
+import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs'
 
 
-    const register = async () => {
-        try {
-            const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
-        } catch (error) {
-            alert((error as Error).message);
-        }
-    }
+interface InputProps {
+    placeholder?: string;
+    className?: string;
+    type: string;
+    eye?: boolean;
+    onchange?: any;
+  }
 
-    const login = async () => {
-        try {
-            const user = await signInWithEmailAndPassword(auth, loginEmail, loginEmail)
-        } catch (error) {
-            alert((error as Error).message);
-        }
-    }
+  const Input: React.FC<InputProps> = ({ placeholder, className, type, eye, onchange }) => {
+    const [toggle, setToggle] = useState(true)
 
-    const logout = async () => {
-        await signOut(auth)
-    }
+    const toggleVisibility = () => {
+      setToggle(prevToggle => !prevToggle); // Toggle the state when the button is clicked
+    };
 
-    const changeForm = () => {
-        console.log('Button clicked');
-        setIsLoginMode(!isLoginMode);
-    }
+    const inputType = toggle ? type : 'text';
 
-  return (
-    <>
-    <div className='flex flex-col gap-6 w-[27rem] border border-[--border] shadow-xl p-7 rounded-xl pb-8'>
-    {isLoginMode ? (
-    <>
-        <div>
-            <h1 className='text-2xl font-medium'>Sign in to your account</h1>
-            <p className='text-[--highlight]'>Enter your email below to login your account</p>
-        </div>
+    return (
+      <div className='flex border border-[--border] p-2 rounded-lg w-full'>
+        <input type={inputType} onChange={onchange} className={`bg-transparent items-center focus:border-white-60 w-full placeholder:text-[--highlight] outline-none ${className}`}
+          placeholder={placeholder} />
 
-        <div className='flex flex-col justify-start items-center w-full'>
-            <Button icon={<AiOutlineGoogle />} title="Google" />
-            <div className="inline-flex items-center justify-center w-full pt-2">
-                <hr className="h-px my-5 border-0 bg-[--border] w-full" />
-                <span className="absolute px-3 font-medium -translate-x-1/2 bg-[--bg] text-[--highlight] left-1/2 ">OR CONTINUE WITH</span>
-            </div>
-        </div>
-
-        <div>
-            <h1 className='text-lg font-medium'>Email</h1>
-            <Input type='email' placeholder="Enter your email here" eye={false} onchange={(event: any) => setRegisterEmail(event.target.value)}/>
-        </div>
-        <div>
-            <h1 className='text-lg font-medium'>Password</h1>
-            <Input type='password' placeholder="Enter your password here" eye={true} onchange={(event: any) => setRegisterPassword(event.target.value)}/>
-        </div>
-        <Button onClick={register}>Sign In</Button>
-        <p className='text-sm text-stone-400 gap-1 flex'>Dont have an account?<button className='text-sm text-stone-400 underline' onClick={changeForm}>Register</button></p>
-    </>
-    ) : (
-    <>
-        <div>
-            <h1 className='text-2xl font-medium'>Create an account</h1>
-            <p className='text-[--highlight]'>Enter your email below to create your account</p>
-        </div>
-
-        <div className='flex flex-col justify-start items-center w-full'>
-            <Button icon={<AiOutlineGoogle />} title="Google" />
-            <div className="inline-flex items-center justify-center w-full pt-2">
-                <hr className="h-px my-5 border-0 bg-[--border] w-full" />
-                <span className="absolute px-3 font-medium -translate-x-1/2 bg-[--bg] text-[--highlight] left-1/2 ">OR CONTINUE WITH</span>
-            </div>
-        </div>
-
-        <div>
-            <h1 className='text-lg font-medium'>Email</h1>
-            <Input type='email' placeholder="Enter your email here" eye={false} onchange={(event: any) => setRegisterEmail(event.target.value)}/>
-        </div>
-        <div>
-            <h1 className='text-lg font-medium'>Password</h1>
-            <Input type='password' placeholder="Enter your password here" eye={true} onchange={(event: any) => setRegisterPassword(event.target.value)}/>
-        </div>
-        <Button onClick={register}>Sign Up</Button>
-        <p className='text-sm text-stone-400 gap-1 flex'>Already have an account?<button className='text-sm text-stone-400 underline' onClick={changeForm}>Login</button></p>
-    </>
-    )}
-    </div>
-    </>
-  )
-}
+        {eye ? (
+          <button onClick={toggleVisibility} className='text-[--highlight] hover:text-white transition'>
+            {toggle ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
+          </button>
+        ) : (
+          <></>
+        )}
+      </div>
+        
+    );
+  };
+  
+  export default Input;
--- components/Login.tsx
+++ components/Login.tsx
@@ -1,65 +1,65 @@
 "use client"
 
-//Firebase
-import { collection, getDocs, addDoc, query, where } from 'firebase/firestore';
-import { auth, db } from "@/utils/firebase";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { useAuthState } from "react-firebase-hooks/auth";
-//
-import React from 'react'
+import { useState } from 'react'
 import Input from './Input'
 import Button from './Button'
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import { AiOutlineGoogle } from 'react-icons/ai'
-import { message } from 'antd';
-import { useRouter } from "next/navigation";
+import { auth } from '@/utils/firebase'
+
 
 export default function Login() {
-    const router = useRouter()
-    const [user, loading] = useAuthState(auth);
 
-    const googleProvider = new GoogleAuthProvider();
-    
-    const handleLogin = async () => {
+    const [user, setUser] = useState({})
+
+    const [registerEmail, setRegisterEmail] = useState("")
+    const [registerPassword, setRegisterPassword] = useState("")
+
+    const [loginEmail, setLoginEmail] = useState("")
+    const [loginPassword, setLoginPassword] = useState("")
+
+    const [isLoginMode, setIsLoginMode] = useState(false); // Add this state
+
+
+    const register = async () => {
         try {
-          // Google Auth
-          const res = await signInWithPopup(auth, googleProvider);
-          message.success("Signed in successfully");
-
-          //Router
-          router.push('/dashboard');
-    
-          // User
-          const userRef = collection(db, "users");
-          const q = query(userRef, where("email", "==", res.user.email));
-          const querySnapshot = await getDocs(q);
-    
-          // Firestore
-          if (querySnapshot.empty) {
-            const docRef = await addDoc(userRef, {
-              name: res.user.displayName,
-              email: res.user.email,
-            });
-            console.log("Document written with ID:", docRef.id);
-          } else {
-            console.log("User exists");
-          }
-        } catch (err) {
-          message.error("Error signing in");
-          console.error(err);
+            const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+        } catch (error) {
+            alert((error as Error).message);
         }
-      };
+    }
 
-    return (
-    <div className='flex flex-col gap-6 border border-[--border] shadow-xl p-7 rounded-xl pb-8'>
+    const login = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(auth, loginEmail, loginEmail)
+        } catch (error) {
+            alert((error as Error).message);
+        }
+    }
+
+    const logout = async () => {
+        await signOut(auth)
+    }
+
+    const changeForm = () => {
+        console.log('Button clicked');
+        setIsLoginMode(!isLoginMode);
+    }
+
+  return (
+    <>
+    <div className='flex flex-col gap-6 w-[27rem] border border-[--border] shadow-xl p-7 rounded-xl pb-8'>
+    {isLoginMode ? (
+    <>
         <div>
-            <h1 className='text-2xl font-medium'>Create an account</h1>
-            <p className='text-[--highlight]'>Enter your email below to create your account</p>
+            <h1 className='text-2xl font-medium'>Sign in to your account</h1>
+            <p className='text-[--highlight]'>Enter your email below to login your account</p>
         </div>
 
         <div className='flex flex-col justify-start items-center w-full'>
-            <Button onClick={handleLogin}>
-                <AiOutlineGoogle />
-                Google
+        <Button>
+              <AiOutlineGoogle />
+              Google
             </Button>
             <div className="inline-flex items-center justify-center w-full pt-2">
                 <hr className="h-px my-5 border-0 bg-[--border] w-full" />
@@ -69,15 +69,46 @@ export default function Login() {
 
         <div>
             <h1 className='text-lg font-medium'>Email</h1>
-            <Input type='email' placeholder="Enter your email here" eye={false}/>
+            <Input type='email' placeholder="Enter your email here" eye={false} onchange={(event: any) => setRegisterEmail(event.target.value)}/>
         </div>
         <div>
             <h1 className='text-lg font-medium'>Password</h1>
-            <Input type='password' placeholder="Enter your password here" eye={true}/>
+            <Input type='password' placeholder="Enter your password here" eye={true} onchange={(event: any) => setRegisterPassword(event.target.value)}/>
         </div>
-        <Button className='bg-white text-black'>
-            Create account
-        </Button>
+        <Button onClick={register}>Sign In</Button>
+        <p className='text-sm text-stone-400 gap-1 flex'>Dont have an account?<button className='text-sm text-stone-400 underline' onClick={changeForm}>Register</button></p>
+    </>
+    ) : (
+    <>
+        <div>
+            <h1 className='text-2xl font-medium'>Create an account</h1>
+            <p className='text-[--highlight]'>Enter your email below to create your account</p>
+        </div>
+
+        <div className='flex flex-col justify-start items-center w-full'>
+            <Button>
+              <AiOutlineGoogle />
+              Google
+            </Button>
+            <div className="inline-flex items-center justify-center w-full pt-2">
+                <hr className="h-px my-5 border-0 bg-[--border] w-full" />
+                <span className="absolute px-3 font-medium -translate-x-1/2 bg-[--bg] text-[--highlight] left-1/2 ">OR CONTINUE WITH</span>
+            </div>
+        </div>
+
+        <div>
+            <h1 className='text-lg font-medium'>Email</h1>
+            <Input type='email' placeholder="Enter your email here" eye={false} onchange={(event: any) => setRegisterEmail(event.target.value)}/>
+        </div>
+        <div>
+            <h1 className='text-lg font-medium'>Password</h1>
+            <Input type='password' placeholder="Enter your password here" eye={true} onchange={(event: any) => setRegisterPassword(event.target.value)}/>
+        </div>
+        <Button onClick={register}>Sign Up</Button>
+        <p className='text-sm text-stone-400 gap-1 flex'>Already have an account?<button className='text-sm text-stone-400 underline' onClick={changeForm}>Login</button></p>
+    </>
+    )}
     </div>
+    </>
   )
 }
