aeb681883ba86e5f532e247aa2847f6cf6a7fab6
DarKS4ys <melihyardim1057@gmail.com>
2023-08-29 11:36:47 +0300

    fixed the auth, added name storing

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,11 +1,28 @@
 "use client"
 
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '@/utils/firebase';
+import { auth, db } from '@/utils/firebase'; // Make sure you have imported the 'db' from '@/utils/firebase'
+import { useEffect, useState } from 'react';
+import { collection, query, getDocs, where } from 'firebase/firestore'; // Import Firestore functions
 
 export default function Dashboard() {
   const [user, loading] = useAuthState(auth);
+  const [userName, setUserName] = useState(null);
 
+  useEffect(() => {
+    if (user) {
+      // Get user's document from Firestore based on their email
+      const userRef = collection(db, 'users');
+      const q = query(userRef, where('email', '==', user.email));
+      getDocs(q).then((querySnapshot) => {
+        if (!querySnapshot.empty) {
+          // Retrieve and set the user's name from the Firestore document
+          const userData = querySnapshot.docs[0].data();
+          setUserName(userData.name);
+        }
+      });
+    }
+  }, [user]);
 
   return (
     <div className='h-screen flex flex-col ml-[16rem] m-4'>
@@ -13,9 +30,10 @@ export default function Dashboard() {
         <h1 className='text-2xl font-bold'>Dashboard</h1>
       </div>
       <div className="flex flex-col">
-        <p>Welcome,</p>{user?.displayName}
-        <p>Your email address:{user?.email}</p>
-      </div>
+  <p>Welcome, {userName ? userName : 'loading...'} </p>
+  <p>Your email address: {user ? user.email : 'loading...'}</p>
+</div>
+
     </div>
   )
 }
