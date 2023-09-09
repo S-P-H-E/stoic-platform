2a9e28ba58aa334bb6e152c20c1c49701e4a7520
S-P-H-E <sphe.g.personal@gmail.com>
2023-09-09 22:17:15 +0200

    lesson selection

--- app/(pages)/[courseId]/[lessonId]/page.tsx
+++ app/(pages)/[courseId]/[lessonId]/page.tsx
@@ -10,6 +10,7 @@ import Script from 'next/script';
 import Comments from '@/components/Comments';
 import { UserDataFetcher } from '@/utils/userDataFetcher';
 import {motion} from 'framer-motion'
+import { AiOutlineLink } from 'react-icons/ai'
 
 
 interface LessonItem {
@@ -27,9 +28,13 @@ export default function LessonPage() {
 
   const {user, userId } = UserDataFetcher()
 
-  let currentLessonIndex = -1;
+  
   
   const pathname = usePathname();
+
+  const lessonpath = useParams()
+
+  console.log("Lesson Path: ", lessonpath.lessonId, " pathname: ", pathname)
 
   console.log('Current pathname: ' + pathname)
 
@@ -94,6 +99,7 @@ export default function LessonPage() {
           lessonsData.sort((a, b) => a.order - b.order);
           
           setLessons(lessonsData);
+          
         } else {
           console.log('Course Id not found')
         }
@@ -104,6 +110,7 @@ export default function LessonPage() {
 
     fetchLessonData();
     fetchLessonsForCourse();
+    
   }, [courseId, lessonId, userId]);
 
   if (!lesson || !lessons) {
@@ -120,10 +127,10 @@ export default function LessonPage() {
         <Search />
       </div>
 
-      <div className="flex p-10">
+      <div className="flex flex-col md:flex-row p-10">
         <div>
             <>
-              <div className='w-[1024px] h-[576px] rounded-3xl shadow-2xl shadow-white/10 transition ring-[--border] hover:ring-4 hover:ring-offset-4 ring-offset-[--bg]'>
+              <div className=' md:w-[1024px] rounded-3xl shadow-2xl shadow-white/10 aspect-video'>
                 <iframe
                   src={lesson.url}
                   allow="autoplay; fullscreen; picture-in-picture"
@@ -133,12 +140,20 @@ export default function LessonPage() {
               </div>
               <Script src="https://player.vimeo.com/api/player.js" />
 
-              <div className='my-5 mb-20 border border-[#1E1E1E] rounded-2xl p-5'>
-                <h1 className='text-3xl font-medium'>{lesson.title}</h1>
+              <div className='my-5 md:mb-20 border border-[--border] rounded-2xl p-5'>
+                <div className='flex justify-between'>
+                  <h1 className='text-3xl font-medium'>{lesson.title}</h1>
+                  <button className='border border-[--border] flex gap-1 h-fit items-center rounded-xl px-2'>
+                    <AiOutlineLink />
+                    Link
+                  </button>
+                </div>
                 <p className='rounded-xl mt-3'>{lesson.description}</p>
               </div>
               
-              <Comments courseId={courseId as string} lessonId={lessonId as string}/> {/* I want to return the lessons document id here */}
+              <div className='hidden md:block'>
+                <Comments courseId={courseId as string} lessonId={lessonId as string}/>
+              </div>
             </>
         </div>
 
@@ -154,14 +169,17 @@ export default function LessonPage() {
               once: true,
             }}
             >
-            <Link href={`/${courseId}/${lessonItem.id}`} key={index} className='cursor-pointer'>
-                <div className={`mx-5 px-3 py-3 rounded-2xl transition-all border border-[#1E1E1E] hover:scale-105 cursor-pointer flex justify-start items-center gap-2 ${index === currentLessonIndex ? 'bg-[#1E1D1E]' : ''}`}>
+            <Link href={`/${courseId}/${lessonItem.id}`} key={index} className='cursor-pointer w-full'>
+                <div className={`mx-5 px-3 py-3 rounded-2xl transition-all bg-[--bg] border border-[--border] hover:scale-105 cursor-pointer flex justify-start items-center gap-2 ${String(lessonpath.lessonId) === String(lessonItem.id) ? 'invert' : ''}`}>
                     <p className='text-3xl font-mono rounded-full p-2 px-4'>{lessonItem.order as unknown as string}</p>
                     <h1 className='text-xl font-medium'>{lessonItem.title}</h1>
                 </div>
             </Link>
             </motion.div>
             ))}
+            <div className='visible md:hidden'>
+                <Comments courseId={courseId as string} lessonId={lessonId as string}/>
+              </div>
           </div>
         </div>
       </div>
