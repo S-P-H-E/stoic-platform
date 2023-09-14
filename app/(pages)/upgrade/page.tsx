72e92be1e2ffbf389f955c36a30a3d82c505e261
S-P-H-E <sphe.g.personal@gmail.com>
2023-09-14 20:04:55 +0200

    push

--- app/(pages)/upgrade/page.tsx
+++ app/(pages)/upgrade/page.tsx
@@ -1,26 +1,95 @@
+"use client"
 import React from 'react'
-import { FaBook } from 'react-icons/fa'
+import { FaBook, FaStripe } from 'react-icons/fa'
+import { BsFillPersonFill, BsStars, BsFillCheckCircleFill, BsPeopleFill } from 'react-icons/bs'
+import clsx from 'clsx'
+import { motion } from 'framer-motion'
+import { UserDataFetcher } from '@/utils/userDataFetcher'
 
 export default function Upgrade() {
+  const { userName } = UserDataFetcher()
+
+  const fadeInAnimationVariants = { // for framer motion  
+    initial: {
+        opacity: 0,
+        y: 100,
+    },
+    animate: (index: number) => ({
+        opacity: 1,
+        y: 0,
+        transition: {
+            delay: 0.05 * index,
+        }
+    })
+}
+
   const features = [
     {
+      id: 1,
       icon: <FaBook size={25}/>,
-      name: 'course'
-    }
+      name: 'Courses'
+    },
+    {
+      id: 2,
+      icon: <BsStars size={25}/>,
+      name: 'AI Tools'
+    },
+    {
+      id: 3,
+      icon: <BsFillPersonFill size={25}/>,
+      name: '1-on-1 Coaching'
+    },
+    {
+      id: 4,
+      icon: <BsPeopleFill size={25}/>,
+      name: 'Community'
+    },
   ]
+
   return (
     <div className='flex justify-center items-center h-screen'>
-      <div className='border border-[#1C1C1D] w-[500px] rounded-3xl p-8 flex flex-col gap-2'>
-        <div className='flex justify-center items-center w-full'>
-          <h1 className='text-xl'>Upgrade to Stoic</h1>
-          <p className='bg-gradient-to-tr from-[#5952da] to-[#39358d] rounded-full px-4 py-1 mx-3'>PREMIUM</p>
+      <motion.div className='border border-[#1C1C1D] w-[450px] rounded-3xl p-8 flex flex-col items-center gap-2 bg-gradient-to-tl from-[white]/5'
+        custom={1}
+        variants={fadeInAnimationVariants}
+        initial="initial"
+        whileInView="animate"
+        viewport={{
+          once: true,
+        }}
+      >
+        <div className='flex flex-col justify-center items-center w-full'>
+          {/* <div className='flex items-center gap-1 bg-[--upgrade] rounded-full px-4 py-1 text-black my-2'>
+            <BsFillPersonFill />
+            <h2 className='text-black'>{userName}</h2>
+          </div> */}
+          <h1 className='text-2xl uppercase'>Upgrade to premium</h1>
         </div>
 
-        <div className='flex items-center gap-1 w-full p-2 rounded-md border border-[#1C1C1D]'>
-          
-          <h1>Courses</h1>
+        <h1 className='text-4xl text-[--upgrade] pb-5 flex flex-col items-center font-medium'>
+          <mark className='bg-transparent text-xl text-gray-500 line-through'>$99.99</mark>$49.99
+        </h1>
+
+        {features.map((feature) => (
+          <div key={feature.id} className='flex justify-between items-center gap-1 w-full p-2 rounded-md border border-[#1C1C1D]'>
+            <div className='flex items-center'>
+              <div className='bg-[#F7C910]/10 text-[--upgrade] p-2 rounded-lg'>
+                {feature.icon}
+              </div>
+              <h1 className='px-2 font-medium'>{feature.name}</h1>
+            </div>
+            
+            <BsFillCheckCircleFill className='text-[--upgrade]'/>
+          </div>
+        ))}
+        <button className='upgrade mt-5'>
+          UPGRADE
+        </button>
+
+        <div className='flex items-center gap-1 border border-[--border] px-2 rounded-lg mt-6'>
+          <p className='text-sm'>Secured by</p>
+          <FaStripe size={35}/>
         </div>
-      </div>
+      </motion.div>
     </div>
   )
 }
