a5c05745c8f2c5b88eb612443bd3e486f74419a4
DarKS4ys <melihyardim1057@gmail.com>
2023-10-03 21:18:03 +0300

    sidebar added

--- app/(pages)/[courseId]/[lessonId]/page.tsx
+++ app/(pages)/[courseId]/[lessonId]/page.tsx
@@ -447,7 +447,7 @@ export default function LessonPage() {
                   <ContextMenuTrigger>
                   <div className={`w-full md:w-[300px] md:mx-5 px-3 py-3 rounded-2xl transition-all bg-[--bg] border border-[--border] group cursor-pointer flex justify-between items-center gap-2 
                   ${String(lessonpath.lessonId) === String(lessonItem.id) ? 'bg-white text-black' : ''}
-                  ${videoPlaying ? 'bg-green-400' : ''}`}>
+                  ${videoPlaying && String(lessonpath.lessonId) === String(lessonItem.id) ? 'bg-green-400' : ''}`}>
                     <div className="flex items-center">
                       <p className='text-3xl font-mono rounded-full p-2 px-4'>{lessonItem.order as unknown as string}</p>
                       <h1 className={clsx('text-xl font-medium', {
