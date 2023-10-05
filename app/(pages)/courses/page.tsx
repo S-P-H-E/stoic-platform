c4a6155716b7f3e8e565a9855a8a816d973551e3
DarKS4ys <melihyardim1057@gmail.com>
2023-10-06 00:11:46 +0300

    DYNAMIC COURSES LETS GO

--- app/(pages)/courses/page.tsx
+++ app/(pages)/courses/page.tsx
@@ -3,97 +3,114 @@ import { db } from '@/utils/firebase';
 import { UserDataFetcher } from '@/utils/userDataFetcher';
 import { collection, doc, getDoc, getDocs, onSnapshot, orderBy, query } from 'firebase/firestore';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
 import { useCallback, useEffect, useState } from 'react';
 
 export default function Courses() {
-    const router = useRouter()
-    const { userId, userStatus } = UserDataFetcher()
-    const isPremium = userStatus === 'user' || userStatus === 'admin'
+  const { userId, userStatus } = UserDataFetcher()
+  const isPremium = userStatus === 'user' || userStatus === 'admin'
 
-    const [courses, setCourses] = useState<Array<any>>([]);
-    const [userCourses, setUserCourses] = useState<Array<any>>([]);
+  const [courses, setCourses] = useState<Array<any>>([]);
+  const [userCourses, setUserCourses] = useState<Array<any>>([]); // User's watched courses
 
-    function truncateText(text: string, maxLength: number) {
-      if (text.length > maxLength) {
-        return text.substring(0, maxLength) + '...';
-      }
-      return text;
+  function truncateText(text: string, maxLength: number) {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + '...';
     }
-    
-    const fetchLastLessonId = useCallback(async (userId: string | null) => {
+    return text;
+  }
+
+  const fetchSavedLessons = useCallback(() => {
+    if (userId && isPremium) {
       try {
-        const userCoursesRef = collection(db, 'users', String(userId), 'courses');
-        const userCoursesSnapshot = await getDocs(userCoursesRef);
-  
-        const userCoursesData = userCoursesSnapshot.docs.map((userCourseDoc) => ({
-          id: userCourseDoc.id,
-          ...userCourseDoc.data(),
-        }));
-  
-        setUserCourses(userCoursesData);
-  
-        console.log('User Courses:', userCoursesData);
-      } catch (error) {
-        console.error('Error fetching user courses:', error);
+        const userCoursesRef = collection(db, 'users', userId, 'courses');
+
+        const unsubscribe = onSnapshot(userCoursesRef, (querySnapshot) => {
+          const userCoursesData = querySnapshot.docs.map((doc) => doc.data());
+          setUserCourses(userCoursesData);
+        });
+        return () => {
+          unsubscribe();
+        };
+      } catch (err) {
+        console.error(err);
       }
-    }, []);
+    }
+  }, [userId, isPremium]);
 
+  const fetchCourses = useCallback(async () => {
+    if (userId && isPremium) {
+      try {
+        const coursesRef = collection(db, 'courses');
 
-    const fetchCourses = useCallback(async () => {
-      if (userId && isPremium) {
-        try {
-          const coursesRef = collection(db, 'courses');
-    
-          const unsubscribe = onSnapshot(coursesRef, async (snapshot) => {
-            const coursesData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    
-            const coursesWithLessons = await Promise.all(
-              coursesData.map(async (course) => {
-                const lessonsRef = collection(db, 'courses', course.id, 'lessons');
-                const lessonsSnapshot = await getDocs(query(lessonsRef, orderBy('order')));
-    
-                // Get the first lesson by sorting based on the "order" field
-                const firstLesson = lessonsSnapshot.docs[0];
-    
-                return {
-                  ...course,
-                  firstLesson: firstLesson ? { id: firstLesson.id, ...firstLesson.data() } : null,
-                };
-              })
-            );
-    
-            setCourses(coursesWithLessons);
-            console.log(coursesWithLessons); // Log the fetched data
-          });
-    
-          return () => {
-            unsubscribe();
-          };
-        } catch (err) {
-          console.log(err);
-        }
+        const unsubscribe = onSnapshot(coursesRef, async (snapshot) => {
+          const coursesData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+          const coursesWithLessons = await Promise.all(
+            coursesData.map(async (course) => {
+              const lessonsRef = collection(db, 'courses', course.id, 'lessons');
+              const lessonsSnapshot = await getDocs(query(lessonsRef, orderBy('order')));
+
+              const lessonIds = lessonsSnapshot.docs.map((lessonDoc) => lessonDoc.id);
+              const firstLesson = lessonsSnapshot.docs[0];
+
+              let userCourseData = userCourses.find((data) => data.courseId === course.id);
+            
+
+              if (!userCourseData) {
+                const userCourseRef = doc(db, 'users', userId, 'courses', course.id);
+                const userCourseSnapshot = await getDoc(userCourseRef);
+
+                if (userCourseSnapshot.exists()) {
+                  userCourseData = userCourseSnapshot.data();
+                }
+              }
+
+              return {
+                ...course,
+                firstLesson: firstLesson ? { id: firstLesson.id, ...firstLesson.data() } : null,
+                lessonIds: lessonIds,
+                userCourseData: userCourseData || null,
+              };
+            })
+          );
+
+          setCourses(coursesWithLessons);
+        });
+
+        return () => {
+          unsubscribe();
+        };
+      } catch (err) {
+        console.log(err);
       }
-    }, [userId, isPremium]);
-    
-    useEffect(() => {
-      fetchCourses();
-      fetchLastLessonId(userId)
-    }, [fetchCourses, fetchLastLessonId, userId]);
+    }
+  }, [userId, isPremium, userCourses]);
 
-    return (
-        <div className='h-screen flex flex-col gap-4 p-8 md:px-16'>
-            <h1 className='text-3xl font-semibold'>Courses</h1>
+  useEffect(() => {
+    fetchCourses();
+    fetchSavedLessons();
+  }, [fetchCourses, fetchSavedLessons, userId]);
 
+  return (
+    <div className='h-screen flex flex-col gap-4 p-8 md:px-16'>
+      <h1 className='text-3xl font-semibold'>Courses</h1>
 
-            <div className='flex flex-col gap-4'>
-              {courses.map((course) => (
-                <Link href={`/${course.id}/${course.firstLesson.id}`} key={course.id} className="flex flex-col w-fit bg-white text-black rounded-xl p-4">
-                  <p>{truncateText(course.name, 35)}</p>
-                  <p>{truncateText(course.description, 40)}</p>
-                </Link>
-              ))}
-            </div>
-        </div>
-    );
+      <div className='flex flex-col gap-4'>
+        {courses.map((course) => {
+          const lastLessonId = course.userCourseData ? course.userCourseData.lastLessonId : null;
+
+          const href = lastLessonId
+            ? `/${course.id}/${lastLessonId}`
+            : `/${course.id}/`;
+
+          return (
+            <Link href={href} key={course.id} passHref className="flex flex-col w-fit bg-white text-black rounded-xl p-4">
+              <p>{truncateText(course.name, 35)}</p>
+              <p>{truncateText(course.description, 40)}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
