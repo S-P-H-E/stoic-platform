ea7ce06875f26b2fd94fc5f1b1f5415805cdaf9a
DarKS4ys <melihyardim1057@gmail.com>
2023-10-08 11:32:51 +0300

    Fixed build error

--- app/(pages)/courses/page.tsx
+++ app/(pages)/courses/page.tsx
@@ -4,7 +4,7 @@ export default function CoursesPage() {
 
   return (
     <div className='h-screen flex flex-col gap-4 p-8 md:px-16'>
-      <h1 className='text-3xl font-semibold'>Courses</h1>
+      <h1 className='text-3xl font-semibold'>Courses - wip</h1>
         <Courses/>
     </div>
   );
