1775ba94ed8e8620e96a29145afc89f15aa36528
DarKS4ys <melihyardim1057@gmail.com>
2023-11-05 02:29:00 +0300

    i forgot the gap 🐷

--- components/Locked.tsx
+++ components/Locked.tsx
@@ -7,10 +7,12 @@ export default function Locked() {
     <>
     <div className="absolute inset-0 backdrop-blur-xl w-full h-full flex items-center justify-center z-10 rounded-xl">
         <div className="bg-[--bg] opacity-80 absolute inset-0 z-10" />
-        <div className="w-full h-60 text-white text-center p-4 rounded-lg z-20 flex items-center justify-center flex-col">
+        <div className="w-full h-60 text-white text-center p-4 rounded-lg z-20 flex flex-col items-center justify-center">
         <AiFillLock size={64} className="text-yellow-500" />
-        <p className="text-4xl"><Link href={'/upgrade'} className="text-yellow-500 hover:underline font-medium">Upgrade</Link> to get access</p>
-        <Link href={'/upgrade'} className="upgrade bg-yellow-500 !text-black">UPGRADE</Link>
+        <p className="text-4xl pb-2"><Link href={'/upgrade'} className="text-yellow-500 hover:underline font-medium">Upgrade</Link> to get access</p>
+        <Link href={'/upgrade'}>
+          <button className="upgrade bg-yellow-500 !text-black">UPGRADE</button>
+        </Link>
         </div>
     </div>
     </>
