1c26c2120d20ef6af60a8aa119237b5bb9e6b2b5
DarKS4ys <melihyardim1057@gmail.com>
2023-11-05 02:20:42 +0300

    more locked stuff 😊

--- app/(pages)/settings/page.tsx
+++ app/(pages)/settings/page.tsx
@@ -1,9 +1,28 @@
+"use client"
+
+import Locked from "@/components/Locked";
 import AccountSettings from "@/components/Settings/AccountSettings"
+import { UserDataFetcher } from "@/utils/userDataFetcher";
+import { BiLoader } from "react-icons/bi";
 
 export default function Settings() {
-    return (
-      <div className="h-full flex flex-col py-2 px-6 md:py-8 md:px-16 w-full">
-        <AccountSettings />
-      </div>
-    );
+    const { userStatus } = UserDataFetcher();
+
+    if (userStatus == "free") {
+      return (
+        <div className="h-full flex flex-col py-2 px-6 md:py-8 md:px-16 w-full">
+          <Locked/>
+          <AccountSettings />
+        </div>
+      );
+    } else if (userStatus !== undefined && userStatus !== null) {
+      return (
+        <div className="h-full flex flex-col py-2 px-6 md:py-8 md:px-16 w-full">
+          <AccountSettings />
+        </div>
+      );
+    }  else {
+      // Handle the case when userStatus is still loading or unavailable.
+      return <div className="h-screen flex items-center justify-center"><BiLoader size={72} className="animate-spin"/></div>;
+    }
   }
