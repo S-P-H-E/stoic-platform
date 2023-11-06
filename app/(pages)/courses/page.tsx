7f9e016c25b02e2bfd0d610b7aba0e1ff807fb45
DarKS4ys <melihyardim1057@gmail.com>
2023-11-06 21:38:52 +0300

    course fix

--- app/(pages)/courses/page.tsx
+++ app/(pages)/courses/page.tsx
@@ -11,20 +11,25 @@ export default function CoursesPage() {
 
     if (userStatus == 'user') {
       return (
-        <div className='h-full flex flex-col gap-4 md:p-10 md:px-16 p-8 justify-between items-start w-full'>
-              <h1 className='text-3xl font-semibold'>Courses</h1>
-              <CreateCourse className='md:hidden'/>
-              <Courses/>
-            <CreateCourse className='md:block hidden'/>
+        <div className='h-full flex md:p-10 md:px-16 p-8 justify-between items-start w-full'>
+          <Locked/>
+          <div className='flex flex-col gap-4 md:gap-8 w-full'>
+          <h1 className='text-3xl font-semibold'>Courses</h1>
+          <CreateCourse className='md:hidden'/>
+          <Courses/>
+          </div>
+          <CreateCourse className='md:block hidden'/>
         </div>
       )
     } else if (userStatus !== undefined && userStatus !== null && userStatus !== 'free') {
       return (
-        <div className='h-full flex flex-col gap-4 md:p-10 md:px-16 p-8 justify-between items-start w-full'>
-              <h1 className='text-3xl font-semibold'>Courses</h1>
-              <CreateCourse className='md:hidden'/>
-              <Courses/>
-            <CreateCourse className='md:block hidden'/>
+        <div className='h-full flex md:p-10 md:px-16 p-8 justify-between items-start w-full'>
+          <div className='flex flex-col gap-4 md:gap-8 w-full'>
+          <h1 className='text-3xl font-semibold'>Courses</h1>
+          <CreateCourse className='md:hidden'/>
+          <Courses/>
+          </div>
+          <CreateCourse className='md:block hidden'/>
         </div>
       )
     } else {
