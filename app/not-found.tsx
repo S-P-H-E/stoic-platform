f694cba257508e3319d7cf23621db6e576ad9721
DarKS4ys <melihyardim1057@gmail.com>
2023-11-16 20:57:09 +0300

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import { ButtonShad } from '@/components/ui/buttonshad'
+import Image from 'next/image'
+import Link from 'next/link'
+import StoicLogo from '@/public/stoicWhite.webp'
+ 
+export default function NotFound() {
+  return (
+    <div className='flex flex-col h-screen items-center justify-center gap-3'>
+      <Image alt='Not Found' src={StoicLogo} placeholder='blur' className='w-16 h-20 mb-2'/>
+      <h2 className='text-7xl font-medium leading-[3rem]'>404</h2>
+      <h3 className='text-2xl font-medium'>Page not found</h3>
+      <Link href="/">
+        <ButtonShad variant={'outline'}>Take me back to homepage</ButtonShad>
+      </Link>
+    </div>
+  )
+}
