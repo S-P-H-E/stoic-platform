e6edc30a4082764885cd09119267903c00e953f3
DarKS4ys <melihyardim1057@gmail.com>
2023-12-30 23:16:35 +0300

    confetti & fixes

--- app/upgrade/Upgraded.tsx
+++ app/upgrade/Upgraded.tsx
@@ -1,9 +1,52 @@
-import { ButtonShad } from '@/components/ui/buttonshad'
-import Image from 'next/image'
-import Link from 'next/link'
-import StoicLogo from '@/public/stoicWhite.webp'
- 
+"use effect"
+import { ButtonShad } from '@/components/ui/buttonshad';
+import Image from 'next/image';
+import Link from 'next/link';
+import StoicLogo from '@/public/stoicWhite.webp';
+import Confetti from 'react-confetti';
+import React, { useEffect, useState } from 'react';
+
 export default function Upgraded() {
+  const [confettiActive, setConfettiActive] = useState(true);
+  const [confettiOpacity, setConfettiOpacity] = useState(1);
+  const [confettiDimensions, setConfettiDimensions] = useState({
+    width: 0,
+    height: 0
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setConfettiDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    };
+
+    setConfettiDimensions({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const fadeOutTimeout = setTimeout(() => {
+      setConfettiOpacity(0);
+      setTimeout(() => {
+        setConfettiActive(false);
+      }, 1000);
+    }, 3000);
+
+    return () => {
+      clearTimeout(fadeOutTimeout);
+    };
+  }, []);
+
   return (
     <div className='flex flex-col h-screen items-center justify-center gap-3'>
       <Image alt='Stoic Logo' src={StoicLogo} placeholder='blur' className='w-16 h-20 mb-2'/>
@@ -12,6 +55,14 @@ export default function Upgraded() {
       <Link href="/">
         <ButtonShad variant={'outline'}>Go to dashboard</ButtonShad>
       </Link>
+
+      {confettiActive && (
+        <Confetti
+          width={confettiDimensions.width}
+          height={confettiDimensions.height}
+          style={{ opacity: confettiOpacity, transition: 'opacity 1s ease-out' }}
+        />
+      )}
     </div>
-  )
+  );
 }
